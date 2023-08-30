ebf0a4e1840a39b7c521dccfd1cecaf2f6ca195c
Bryan Robinson <bryanlrobinson@gmail.com>
2023-08-30 16:16:47 -0400

    Adds devto endpoints

---
+++ src/app/api/devto/articles/[username]/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+// make GET route for nextjs
+
+export async function GET(request, {params}) {
+    const data = await fetch(`https://dev.to/api/articles?username=${params.username}`)
+    const body = await data.json()
+    console.log(body)
+    // return products based on nextjs return
+    return NextResponse.json({ data: body })
+
+}
+ 
+
+
